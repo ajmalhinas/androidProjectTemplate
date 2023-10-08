618c595371f3d9df13aa6a37c5e703d075a4801e
ajmal <ajmal@ajmal.com>
2023-10-08 23:07:41 +0530

    writing my first action

---
+++ app/src/main/java/school/actions/StudentRegUIFind.java
@@ -0,0 +1,10 @@
+package school.actions;
+
+import com.androix.FBaseAction;
+
+public class StudentRegUIFind extends FBaseAction {
+    @Override
+    public void execute() throws Exception {
+        addMessage("msg","My first Action");
+    }
+}
